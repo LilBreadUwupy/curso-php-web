090984822d36e35cb583895580089e2af61b5d88
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-18 18:11:23 -0400

    Simulate database with JSON file

--- add.php
+++ add.php
@@ -1,11 +1,23 @@
-<?php
-if (file_exists("contacts.json")) {
-  $contacts = json_decode(file_get_contents("contacts.json"), true);
-} else {
-  $contacts = [];
-}
-?>
 
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $contact = [
+      "name" => $_POST["name"],
+      "phone_number" => $_POST["phone_number"]
+    ];
+    
+    if (file_exists("contacts.json")) {
+      $contacts = json_decode(file_get_contents("contacts.json"), true);
+    } else {
+      $contacts = [];
+    }
+    $contacts[] = $contact;
+
+    file_put_contents("contacts.json", json_encode($contacts));
+
+    header("Location: index.php");
+  }
+?>
 
 
 <!DOCTYPE html>
@@ -56,7 +68,7 @@ if (file_exists("contacts.json")) {
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="./index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="./add.php">Add Contact</a>
@@ -65,30 +77,39 @@ if (file_exists("contacts.json")) {
       </div>
     </div>
   </nav>
-
   <main>
-    <div class="container pt-4 p-3">
-      <div class="row">
-        <?php if (count($contacts) == 0): ?>
-          <div class="card card-body text-center">
-            <p>No contacts saved yet</p>
-            <a href="add.php">Add One!</a>
-          </div>
-        <?php endif ?>
-        <?php foreach ($contacts as $contact) : ?>
-          <div class="col-md-4 mb-3">
-          <div class="card text-center">
+    <div class="container pt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Add New Contact</div>
             <div class="card-body">
-              <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
-              <p class="m-2"><?= $contact["phone_number"] ?></p>
-              <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-              <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
+              <form method="POST" action="add.php">
+                <div class="mb-3 row">
+                  <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+    
+                  <div class="col-md-6">
+                    <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                  </div>
+                </div>
+    
+                <div class="mb-3 row">
+                  <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
+    
+                  <div class="col-md-6">
+                    <input id="phone_number" type="tel" class="form-control" name="phone_number" required autocomplete="phone_number" autofocus>
+                  </div>
+                </div>
+    
+                <div class="mb-3 row">
+                  <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-       <?php endforeach ?>
-
-        
       </div>
     </div>
   </main>
